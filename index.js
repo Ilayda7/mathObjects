805d55191811e046220c2baee027a396db1a821e
ilayda Arslan <110732054+Ilayda7@users.noreply.github.com>
2025-09-19 16:54:42 +0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,23 @@
+// Math is built-in object that provides a collection of properties and methods
+
+let x = 3; //3.21
+let y = 2;
+let z = 1;
+
+//z = Math.round(x);
+//z = Math.floor(x);
+//z = Math.ceil(x);
+//z = Math.trunc(x);
+//z = Math.pow(x, y);
+//z = Math.sqrt(y);
+//z = Math.log(x);
+//z = Math.sin(x);  //x = 45
+//z = Math.cos(x);  //x = 45
+//z = Math.tan(x);  //x = 45
+//z = Math.abs(x);  //x = -3.21
+//z = Math.sign(x);
+let max = Math.max(x, y, z);
+let min = Math.min(x, y, z);
+
+
+console.log(min);
